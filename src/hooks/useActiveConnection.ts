d6fa58001e827a200f3664aef7208ba2fe361937
tom <tomkit@gmail.com>
2023-07-27 16:36:10 -0700

    unused code

--- src/hooks/useActiveConnection.ts
+++
@@ -1,8 +0,0 @@
-import { fetcher } from "@/lib/fetcher";
-import useSWR from "swr";
-
-export function useActiveConnection(customerId: string) {
-  const { data } = useSWR(`/api/get-active-connection`, fetcher);
-
-  return data;
-}
